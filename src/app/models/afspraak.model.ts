bdf1eda71e215662332b206fe1a4e79bae402aa6
babacool126 <babacool126@gmail.com>
2024-03-08 18:28:50 +0100

    afspraak model gewijzigd and and afspraak-list component

--- src/app/models/afspraak.model.ts
+++ src/app/models/afspraak.model.ts
@@ -1,14 +1,11 @@
 // src/app/models/afspraak.model.ts
-    
-  export interface Afspraak {
-    id?: number,
-    klantId?: number;
-    klant: {
-      naam: string;
-      email: string;
-      telefoonnummer: string;
-    } | undefined;
-    soort: number;
-    datumTijd: string;
-  }
-  
+
+import {Klant} from './klant.model'
+
+export interface Afspraak {
+  id: number;
+  klantId: number;
+  klant: Klant;
+  soort: string; // Assuming you manage the enumeration as a string on the front end
+  datumTijd: Date;
+}
